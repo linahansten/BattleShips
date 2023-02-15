1ce92f79ff1913a1715b9dd14d692f84a4b4d0cb
z3keen <alma@henricson.fi>
2023-02-15 11:49:52 +0200

    lobby css

--- createJoin.js
+++ createJoin.js
@@ -1,30 +1,38 @@
-import { db } from "./firebase.js"
-import { ref, push } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
-
-    // send to database and generates a random ID
-    let pushes = await push(ref(db, "lobbies/"),{
-        p1Ready:false,
-        p2Ready:false,
-        connected: false,
-    })
+import { db } from './firebase.js'
+import { ref, push } from 'https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js'
 
 //create lobby button
-const creatEL = document.querySelector(".create-lobby")
+const creatEL = document.querySelector('.create-lobby')
+
 //Adds eventListiner to see if the button is pressed
-creatEL.addEventListener("click", async function(){
+creatEL.addEventListener('click', async function(){
+
     
-    window.location.href = '/game.html?id=' + pushes.key
+	// send to database and generates a random ID
+	let pushes = await push(ref(db, 'lobbies/'),{
+		p1Ready:false,
+		p2Ready:false,
+		connected: false,
+	})
+   
+	window.location.href = '/game.html?id=' + pushes.key
+
+   
 
 })
-//Join lobby button
-const joinEl = document.querySelector("#joinLob")
-let input = document.querySelector("#input")
 
-joinEl.addEventListener("click", function(){
+//Join lobby button
+const joinEl = document.querySelector('#joinLob')
+let input = document.querySelector('#input')
+const joinInput = document.querySelector('.hidden')
+
+joinEl.addEventListener('click', function(){
+	joinInput.classList.remove('hidden')
+	if(input.value){
+		window.location.href = '/game.html?id=' + input.value
+	}
     
-        window.location.href = '/game.html?id=' + input.value
-    
-    
+     
 })
 
 //------------------------------------------------
